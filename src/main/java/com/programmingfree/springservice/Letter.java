a1321d8ff3caf0e6cd0c0c84b5a640c180b29f80
Mark Bordelon <markcbordelon@cloviscorp.com>
2014-12-17 16:28:04 -0800

    back to GreekLetter extending abstract Letter class

--- src/main/java/com/programmingfree/springservice/Letter.java
+++ src/main/java/com/programmingfree/springservice/Letter.java
@@ -5,31 +5,35 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/*
+
 @MappedSuperclass
 @Access(AccessType.PROPERTY)
-*/
 public abstract class Letter { // derive LatinLetter, GreekLetter, etc each with persistence to cat=<lang>. tab=<LETTER>
 
     public enum PhoneticType {
         PUNCT, DIACRITIC, VOWEL, SHORT, LONG, COMMON, DIPHTHONG, SEMIVOWEL, LIQUID, NASAL, LABIAL, DENTAL, GUTTURAL, SIBILANT, ASPIRATE, VOICED;
-        public static boolean contains(String s) {
-            for (PhoneticType choice : values()) if (choice.name().equals(s)) { return true; }
-            return false;
+        public static PhoneticType safeValueOf(String s) {
+            s = s.toUpperCase();
+            for (PhoneticType choice : values()) { if (choice.name().equals(s)) { return choice; } }
+            return null;
         }
     }
+
     public enum PositionType {
-        ALL, INITIAL, MEDIAL, FINAL;  /* initial, medial, final "only" */
-        public static boolean contains(String s) {
-            for (PositionType choice : values()) if (choice.name().equals(s)) { return true; }
-            return false;
+        ALLPOSITION, INITIAL, MEDIAL, FINAL;  /* initial, medial, final "only" */
+        public static PositionType safeValueOf(String s) {
+            s = s.toUpperCase();
+            for (PositionType choice : values()) { if (choice.name().equals(s)) { return choice; } }
+            return null;
         }
     }
+
     public enum CaseType {
-        ALL, LOWER, UPPER;
-        public static boolean contains(String s) {
-            for (CaseType choice : values()) if (choice.name().equals(s)) { return true; }
-            return false;
+        ALLCASE, LOWER, UPPER;
+        public static CaseType safeValueOf(String s) {
+            s = s.toUpperCase();
+            for (CaseType choice : values()) { if (choice.name().equals(s)) { return choice; } }
+            return null;
         }
     }
 
@@ -37,12 +41,13 @@ public abstract class Letter { // derive LatinLetter, GreekLetter, etc each with
     private String name;
     private String translit;
     private String present;
-    private String types;
+
+    /*@JsonInclude*/ private String types;
     private List<PhoneticType> phoneticTypes;
     private CaseType caseType;
     private PositionType positionType;
 
-    //@Id
+    @Id
     public int getId() {
         return id;
     }
@@ -73,24 +78,24 @@ public abstract class Letter { // derive LatinLetter, GreekLetter, etc each with
 
     public String getTypes() {
         StringBuilder sb = new StringBuilder(); //
-        if (phoneticTypes!=null) for (PhoneticType type : phoneticTypes) sb.append(" ").append(type.name());
-        if (caseType!=null) sb.append(" ").append(caseType.name());
-        if (positionType!=null) sb.append(" ").append(positionType.name());
-        return sb.substring( sb.length()>0?1:0 );
+        if (phoneticTypes != null) for (PhoneticType type : phoneticTypes) sb.append(" ").append(type.name());
+        if (caseType != null) sb.append(" ").append(caseType.name());
+        if (positionType != null) sb.append(" ").append(positionType.name());
+        return sb.substring(sb.length() > 0 ? 1 : 0);
     }
     public void setTypes(String types) {
-        List<PhoneticType> phoneticTypes = new ArrayList<PhoneticType>();
-        CaseType caseType = null;
-        PositionType positionType = null;
-        for (String val : Arrays.asList(types.split(" "))) {
-            String canonicalVal = val.toUpperCase();
-            if (PhoneticType.contains(canonicalVal)) phoneticTypes.add(PhoneticType.valueOf(canonicalVal));
-            else if (CaseType.contains(canonicalVal)) caseType = CaseType.valueOf(canonicalVal);
-            else if (PositionType.contains(canonicalVal)) positionType = PositionType.valueOf(canonicalVal);
+        this.phoneticTypes = new ArrayList<PhoneticType>();
+        PhoneticType ph = null;
+        CaseType c = null;
+        PositionType p = null;
+        for (String str : Arrays.asList(types.split(" "))) {
+            ph = PhoneticType.safeValueOf(str);
+            if (ph != null) this.phoneticTypes.add(ph);
+            if (c == null) c = CaseType.safeValueOf(str);
+            if (p == null) p = PositionType.safeValueOf(str);
         }
-        this.phoneticTypes = phoneticTypes; // could be zero-length list
-        this.caseType = (caseType==null)? CaseType.ALL : caseType;
-        this.positionType = (positionType==null)? PositionType.ALL : positionType;
+        this.caseType = ((c == null) ? CaseType.ALLCASE : c);
+        this.positionType = ((p == null) ? PositionType.ALLPOSITION : p);
     }
 
     @Override
--- src/main/java/com/programmingfree/springservice/greek/model/GreekLetter.java
+++ src/main/java/com/programmingfree/springservice/greek/model/GreekLetter.java
@@ -1,116 +1,12 @@
 package com.programmingfree.springservice.greek.model;
 
-//import com.programmingfree.springservice.Letter;
+import com.programmingfree.springservice.Letter;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-/*
-public class GreekLetter extends Letter {
-}
-*/
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
-@Table(name="letters", catalog="clovisdb")
-@Access(AccessType.PROPERTY)
-public class GreekLetter {
-
-    public enum PhoneticType {
-        PUNCT, DIACRITIC, VOWEL, SHORT, LONG, COMMON, DIPHTHONG, SEMIVOWEL, LIQUID, NASAL, LABIAL, DENTAL, GUTTURAL, SIBILANT, ASPIRATE, VOICED;
-        public static PhoneticType safeValueOf(String s) {
-            s = s.toUpperCase();
-            for (PhoneticType choice : values()) if (choice.name().equals(s)) { return choice; }
-            return null;
-        }
-    }
-    public enum PositionType {
-        ALLPOSITION, INITIAL, MEDIAL, FINAL;  /* initial, medial, final "only" */
-        public static PositionType safeValueOf(String s) {
-            s = s.toUpperCase();
-            for (PositionType choice : values()) if (choice.name().equals(s)) { return choice; }
-            return null;
-        }
-    }
-    public enum CaseType {
-        ALLCASE, LOWER, UPPER;
-        public static CaseType safeValueOf(String s) {
-            s = s.toUpperCase();
-            for (CaseType choice : values()) if (choice.name().equals(s)) { return choice; }
-            return null;
-        }
-    }
-
-    private int id;
-    private String name;
-    private String translit;
-    private String present;
-
-    /*@JsonInclude*/ private String types;
-    private List<PhoneticType> phoneticTypes;
-    private CaseType caseType;
-    private PositionType positionType;
-
-    @Id
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTranslit() {
-        return translit;
-    }
-    public void setTranslit(String translit) {
-        this.translit = translit;
-    }
-
-    public String getPresent() {
-        return present;
-    }
-    public void setPresent(String present) {
-        this.present = present;
-    }
-
-    public String getTypes() {
-        StringBuilder sb = new StringBuilder(); //
-        if (phoneticTypes!=null) for (PhoneticType type : phoneticTypes) sb.append(" ").append(type.name());
-        if (caseType!=null) sb.append(" ").append(caseType.name());
-        if (positionType!=null) sb.append(" ").append(positionType.name());
-        return sb.substring( sb.length()>0?1:0 );
-    }
-    public void setTypes(String types) {
-        this.phoneticTypes = new ArrayList<PhoneticType>();
-        PhoneticType ph = null; CaseType c = null; PositionType p = null;
-        for (String str : Arrays.asList(types.split(" "))) {
-            ph = PhoneticType.safeValueOf(str); if (ph!=null) this.phoneticTypes.add(ph);
-            if (c==null) c = CaseType.safeValueOf(str);
-            if (p==null) p = PositionType.safeValueOf(str);
-        }
-        this.caseType = ((c==null)? CaseType.ALLCASE : c);
-        this.positionType = ((p==null)? PositionType.ALLPOSITION : p);
-    }
-
-    @Override
-    public String toString() {
-        return "Letter{#" + id +
-                ", translit='" + translit + '\'' +
-                ", present='" + present + '\'' +
-                ", types={" + getTypes() + "}" +
-                '}';
-    }
-
-}
+@Table(name="letters", catalog="clovisdb") // rename catalog to greek , i.e. <LANG>
+public class GreekLetter extends Letter {}
 
 
